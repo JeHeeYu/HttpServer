e540b6f11477739d7a24d933f00ff1ef48981a66
JeHeeYu <sees1234558@gmail.com>
2023-08-05 15:35:56 +0900

    Project Init

---
+++ README.md
@@ -0,0 +1,8 @@
+
+```
+npm install express
+```
+
+```
+node app.js
+```
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const getRouter = require('./src/routes/get');
+const postRouter = require('./src/routes/post');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use('/get', getRouter);
+app.use('/post', postRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'), '번 포트에서 대기 중')
+});
